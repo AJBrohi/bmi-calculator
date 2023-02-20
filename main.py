59e9ce27ebaaf24e305d7ff04b0cf8e2164f3028
AJBrohi <brohi96@gmail.com>
2023-02-20 22:26:57 +0600

    added more conditions and results

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 print("\nWelcome to my BMI Calculator\n")
 
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
 
-bmi = float(weight) / float(height) ** 2
+bmi = weight / height ** 2
 
 print(f"\nYour BMI is: {round(bmi)}")
 
 if bmi < 18.5:
     print("You're Underweight")
-
 elif bmi < 25:
     print("You're Normal Weight")
-    
 elif bmi < 30:
-    print("You're Overweight")
-    
-else:
+    print("You're Slightly Overweight")
+elif bmi < 35:
     print("You're Obese")
+else:
+    print("You're Clinically Obese")
